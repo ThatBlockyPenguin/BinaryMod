714b45d98745f4c87ea53772ba5e556f063a5a22
BlockyPenguin <me@blockypenguin.com>
2022-04-05 15:38:05 +0100

    Add documentation

--- mod.ts
+++ mod.ts
@@ -8,22 +8,45 @@ function reverseString(str: string) {
   return str.split('').reverse().join('');
 }
 
+/**
+ * The BinaryData class is the default (and only) export of the BinaryMod module.
+ * It holds a string of binary data and allows for manipulation of its contents.
+ */
 export default class BinaryData {
   private data = 0n;
 
+  /**
+   * @param length The length of the binary string to store.
+   * @param data The data to store. This can be a bigint, an integer number, or an integer number stored in a string. Prefixes such as 0x, 0b, and 0o are allowed.
+   */
   constructor(public readonly length: bigint, data: BinaryDataValueTypes) {
     this.set(data);
   }
 
+  /**
+   * Useful for resizing BinaryData objects
+   * @param data A pre-existing BinaryData object.
+   * @param newLength The new length of the binary string. Must not be smaller than the length of `data`.
+   * @returns A new BinaryData object with a length of `length`, containing the data of `data`.
+   */
   public static fromBinaryData(data: BinaryData, newLength: bigint) {
     return new BinaryData(newLength, '0b' + data.toString());
   }
 
+  /**
+   * Sets the data of this object equal to `data`.
+   * @param data The data to store. This can be a bigint, an integer number, or an integer number stored in a string. Prefixes such as 0x, 0b, and 0o are allowed.
+   */
   public set(data: BinaryDataValueTypes) {
     this.data = BigInt(data);
     this.checkLength();
   }
 
+  /**
+   * Sets the bit it index `idx` to `value`.
+   * @param idx The index of the bit to set.
+   * @param value The value that the bit should be set to.
+   */
   public setBitAt(idx: bigint, value: boolean) {
     const thisString = reverseString(this.toString());
     const idxAsNum = bigintToNumber(idx);
@@ -42,10 +65,16 @@ export default class BinaryData {
       throw new Error(`Binary data has invalid length - ${data.length} bits / ${this.length} bits`);
   }
 
+  /**
+   * Checks that the length of the binary string is equal than the maximum allowed bit-length for this object. If not, then an error is thrown.
+   */
   public checkLength() {
     this.toString();
   }
 
+  /**
+   * @returns The binary string stored in this object.
+   */
   public toString() {
     const data = this.data.toString(2).padStart(this.lengthAsNumber(), '0');
     this.checkLengthInternal(data);
@@ -53,6 +82,9 @@ export default class BinaryData {
     return data;
   }
 
+  /**
+   * @returns The binary string stored in this object, with a space inserted every 4 bits from the right.
+   */
   public toPrettyString() {
     let str = reverseString(reverseString(this.toString()).split(/(.{4})/g).join(' '));
 
@@ -62,10 +94,16 @@ export default class BinaryData {
     return str.replaceAll('  ', ' ');
   }
 
+  /**
+   * @returns The length of the binary string stored in this object as a number
+   */
   public lengthAsNumber() {
     return bigintToNumber(this.length);
   }
 
+  /**
+   * @returns The binary string stored in this object as a denary number.
+   */
   public toDenaryNumber() {
     return Number.parseInt(this.toString(), 2);
   }
